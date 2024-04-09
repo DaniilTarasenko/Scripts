7d04d6d352a074b3533e16469eea2cf622eeb955
Daniil Tarasenko <80252099+DaniilTarasenko@users.noreply.github.com>
2024-04-09 12:57:22 +0300

    Update Passgen.py

--- Passgen.py
+++ Passgen.py
@@ -1,34 +1,18 @@
-import secrets
+import time
+import datetime
+import string
 
-while True:
-    
-    print('How many passwords you want')
-    
-    try:
-        count = int(input())
-    except ValueError:
-        print('Error: Wrong value')
-        continue
-    if count>16:
-        print('Error: The amount of passwords you can get must be 16 and less')
-        continue
-    elif count<0:
-        print('Error: The amount of passwords must be a natural number')
-        continue
-    
-    print('Fine, type in the required length of password')
-    
-    try:
-        length = int(input())
-    except ValueError:
-        print('Error: Wrong value')
-        continue
-    if length>16:
-        print('Error: The password must be 16-digits long and less')
-        continue
-    elif length<0:
-        print('Error: The length of a password must be a natural number')
-    
-    for i in range(0, count):
-        password = secrets.token_urlsafe(64)[0:length]
-        print('Password ' + str(i+1) + ': ' + password)
+def password(length):
+    result = ''
+    for i in range(length+1):
+        moment = int(datetime.datetime.now().time().microsecond & 0b01111110)
+        if moment<33: 
+            moment = moment + 33 & 0b01111110
+        result += chr(moment)
+        time.sleep(1)
+    print(result)
+
+print("How long do you want your password to be?")
+length_pas = int(input("Length of password: "))
+for i in range(5): # работает медленно, цикл выставлен, чтобы результат был виден лучше
+    password(length_pas)
