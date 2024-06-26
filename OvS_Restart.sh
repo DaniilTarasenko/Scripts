b84bbf9f7d16e61a7b14412bdd4576d84ceae30a
Daniil Tarasenko <80252099+DaniilTarasenko@users.noreply.github.com>
2024-06-26 11:35:28 +0300

    Update OvS_Restart.sh

--- OvS_Restart.sh
+++ OvS_Restart.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+
+# Суть: бьем службы, с ними вместе автоматом падают процессы, поднимаем службы и проверяем процессы
+# Цель: все необходимые службы перезапущены, поднимаются два процесса.
+# Примечание: 1. намазано много функций sleep для более удобного наблюдения. 2. поднял hostname отдельно, служба появляется сама только через время после запуска демона
+
+
+
+echo "Checking out current OVS state..."
+sleep 2
+
+if systemctl list-units —type=service | grep -q "ovs-vswitchd.service" && systemctl list-units —type=service | grep -q "ovsdb-server.service"; then
+    echo "ovs-vswitchd.service is running."
+    echo "ovsdb-server.service is running."
+    systemctl stop ovsdb-server.service
+    systemctl stop ovs-vswitchd.service
+    systemctl start ovs-vswitchd.service
+    systemctl start ovsdb-server.service
+    echo "OpenVSwitch DB and Daemon have been restarted."
+    echo
+else
+
+    #ветка else не закончена
+    echo "ovs-vswitchd.service is not running."
+fi
+
+
+###ВЕТКА ДЛЯ OVSCLEAR
 #!/usr/bin/env bash
 
 ### vars
@@ -15,14 +43,15 @@ for _bond in ${_bonds[@]}; do
 
   printf "/i/ removing bridge${_bond} ..\n"
   ovs-vsctl del-br ${_bond}
-
-  printf "Stopping OpenVSwitch services"
-  systemctl stop service ovs-vswitchd.service
-  systemctl stop service ovsdb-server.service
-
-  printf "Starting OpenVSwitch services"
-  systemctl start service ovs-vswitchd.service
-  systemctl start ovsdb-server.service
-
   done
+###ВЕТКА ДЛЯ OVSCLEAR
 
+echo "Checking out OvS processes..."
+sleep 2
+if ps -e | grep -q "ovsdb" && ps -e | grep -q "vswitchd"; then
+echo "Both OVS DB and OVS Daemon processes are running"
+ps -e | grep "ovs"
+else
+echo "ERROR: Either OVS DB or OVS Daemon process is not running"
+ps -e | grep "ovs"
+fi
